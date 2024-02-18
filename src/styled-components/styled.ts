66198177df0b7df13b8c81fbf88dfa5d95768996
Abderazak-Amiar <amiar.abderazak@gmail.com>
2024-02-18 23:59:28 +0100

    create styled-components base file

--- src/styled-components/styled.ts
+++ src/styled-components/styled.ts
@@ -1,14 +1,18 @@
 'use client';
-import { Grid, styled } from '@mui/material';
-export const GridHomeContainer = styled(Grid)(() => ({
-  flexDirection: 'column',
-  height: '100vh',
-}));
+import { Box, Grid, Typography, styled } from '@mui/material';
+import { poppins, sfPro_regular } from './fonts';
 
 export const GridSideMenuContainer = styled(Grid)(() => ({
   flexDirection: 'column',
-  alignItems: 'center',
   height: '100vh',
+  justifyContent: 'space-around',
+  fontFamily: sfPro_regular.style.fontFamily,
+  borderRight: 'solid 1px #DBDBDB',
+}));
+
+export const GridMenuContainer = styled(Grid)(() => ({
+  alignItems: 'center',
+  justifyContent: 'space-around',
 }));
 
 export const GridContentContainer = styled(Grid)(() => ({
@@ -19,5 +23,28 @@ export const GridSideMenuLogo = styled(Grid)(() => ({
   display: 'flex',
   flexDirection: 'row',
   marginTop: '48px',
-  justifyContent: 'space-between',
 }));
+
+export const BoxLinksContainer = styled(Box)(() => ({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  marginBlock: '24px',
+}));
+export const GridHomeContainer = styled(Grid)(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100vh',
+}));
+
+export const TypographyPoppins = styled(Typography)(() => ({
+  fontFamily: poppins.style.fontFamily,
+  paddingLeft: '8px',
+}));
+export const TypographyMenuItems = styled(Typography)(() => ({
+  paddingLeft: '8px',
+}));
+
+// export const  = styled()(() => ({
+
+// }));
