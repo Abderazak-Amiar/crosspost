37a979031e1be2b9b1e5ee9995e30ef63147b835
Abderazak-Amiar <amiar.abderazak@gmail.com>
2024-02-18 23:51:52 +0100

    create not-fount component

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div>
+      <Typography>THis Page Not Found</Typography>
+    </div>
+  );
+}
+
+export default NotFound;
