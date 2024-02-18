d75bc3a1cd2046f834307f8b38ea03391e9c9479
Abderazak-Amiar <amiar.abderazak@gmail.com>
2024-02-18 23:51:17 +0100

    add utils folder

---
+++ src/utils/const.ts
@@ -0,0 +1,11 @@
+import { sideMenuItemPropsType } from './types';
+
+export const menu_items = [
+  { icon: 'add', text: 'New post' },
+  { icon: 'home', text: 'Home' },
+  { icon: 'messages', text: 'Messages' },
+  { icon: 'calendar', text: 'Calendar' },
+  { icon: 'auto_post', text: 'Auto post' },
+  { icon: 'analytics', text: 'Analytics' },
+  { icon: 'profiles', text: 'Profiles' },
+];
---
+++ src/utils/types.ts
@@ -0,0 +1,3 @@
+export type sideMenuItemPropsType = {
+  item: { icon: string; text: string };
+};
