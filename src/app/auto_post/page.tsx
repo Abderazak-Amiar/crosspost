9424e72e367c6a55d7c96c2ba2b7be9ba5bc70a8
Abderazak-Amiar <amiar.abderazak@gmail.com>
2024-02-21 12:36:47 +0100

    create routes

---
+++ src/app/auto_post/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function page() {
+  return (
+    <div>
+      <h1>Auto post</h1>
+    </div>
+  );
+}
+
+export default page;
---
+++ src/app/newpost/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+import Content from '@/components/Content';
+import React from 'react';
+
+function page() {
+  return <Content />;
+}
+
+export default page;
