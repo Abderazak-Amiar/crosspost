1c3cafc8cc46bd2e478f152029460d22df51f900
Abderazak-Amiar <amiar.abderazak@gmail.com>
2024-03-02 22:26:48 +0100

    implements cover title component

---
+++ src/components/Cover/CoverTitle.tsx
@@ -0,0 +1,13 @@
+import { CoverTypographyTitle } from '@/styled-components/styled';
+import React from 'react';
+
+function CoverTitle() {
+  return (
+    <CoverTypographyTitle>
+      Choose how your short video will show up for others. Select a frame from
+      your video or photo from your camera roll as a cover image.
+    </CoverTypographyTitle>
+  );
+}
+
+export default CoverTitle;
